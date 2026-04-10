d7979a452e31c070c002e244788d18494e0f58ac
Shubham Bansal <63403639+shubhambansal11-code@users.noreply.github.com>
2026-04-10 14:48:01 +0000

    Improve visualisation

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -25,12 +25,12 @@ z_events=pd.read_csv("outputs/z_events.csv", parse_dates=["start_time", "end_tim
 
 st.sidebar.header("Inputs")
 #model_choice = st.sidebar.selectbox("Select Model",["Autoencoder","Isolation Forest","Z-score"])
-show_if=st.sidebar.checkbox("Compare with Isolation Forest", value=True)
-show_z=st.sidebar.checkbox("Compare with Z-score", value=True) 
+show_if=st.sidebar.checkbox("Compare with Isolation Forest", value=False)
+show_z=st.sidebar.checkbox("Compare with Z-score", value=False) 
 
-date_min=residual.index.min().date()
-date_max=residual.index.max().date()
-date_range=st.sidebar.date_input("Select Date Range", value=(date_min, date_max),min_value=date_min,max_value=date_max)
+# date_min=residual.index.min().date()
+# date_max=residual.index.max().date()
+# date_range=st.sidebar.date_input("Select Date Range", value=(date_min, date_max),min_value=date_min,max_value=date_max)
 
 #Explanations in the dashboard need to be added..
 st.sidebar.markdown("""
@@ -56,16 +56,16 @@ st.sidebar.markdown("""
 4. In the time-series plot, markers indicate the start time of each detected event.
 """)
 
-if isinstance(date_range, tuple) and len(date_range)==2:
-    start_date=pd.to_datetime(date_range[0])
-    end_date=pd.to_datetime(date_range[1])
-else:
-    start_date=pd.to_datetime(date_min)
-    end_date=pd.to_datetime(date_max)
+# if isinstance(date_range, tuple) and len(date_range)==2:
+#     start_date=pd.to_datetime(date_range[0])
+#     end_date=pd.to_datetime(date_range[1])
+# else:
+#     start_date=pd.to_datetime(date_min)
+#     end_date=pd.to_datetime(date_max)
 
-if residual.index.tz is not None:
-    start_date=start_date.tz_localize(residual.index.tz)
-    end_date=end_date.tz_localize(residual.index.tz)
+# if residual.index.tz is not None:
+#     start_date=start_date.tz_localize(residual.index.tz)
+#     end_date=end_date.tz_localize(residual.index.tz)
 
 #Title and Description
 st.title("OPSD Anomaly Detection")
@@ -76,14 +76,19 @@ using data from Open Power System Data (OPSD).
 The main view shows the deep leaning autoencoder model to detect anomalous events. 
 Comparison with Isolation Forest and Z-score methods can be enabled from the sidebar.
 
-Anomalies represent significant deviations between actual and forecasted load.
+Anomalies represent significant deviations between actual and forecasted load. 
 """)
 
-ae_events_filtered=ae_events[(ae_events["start_time"]>=start_date) & (ae_events["start_time"]<=end_date)].copy()
-if_events_filtered=if_events[(if_events["start_time"]>=start_date) & (if_events["start_time"]<=end_date)].copy()
-z_events_filtered=z_events[(z_events["start_time"]>=start_date) & (z_events["start_time"]<=end_date)].copy()
+# ae_events_filtered=ae_events[(ae_events["start_time"]>=start_date) & (ae_events["start_time"]<=end_date)].copy()
+# if_events_filtered=if_events[(if_events["start_time"]>=start_date) & (if_events["start_time"]<=end_date)].copy()
+# z_events_filtered=z_events[(z_events["start_time"]>=start_date) & (z_events["start_time"]<=end_date)].copy()
+# residual_filtered=residual[(residual.index>=start_date) & (residual.index<=end_date)].copy()
 
-residual_filtered=residual[(residual.index>=start_date) & (residual.index<=end_date)].copy()
+#use full saved data directly
+residual_plot=residual.copy()
+ae_events_plot=ae_events.copy()
+if_events_plot=if_events.copy()
+z_events_plot=z_events.copy()
 
 # st.subheader("Overview")
 # ae_summary_row = summary[summary["model"]=="autoencoder"]
@@ -98,48 +103,49 @@ st.subheader("Residual Time Series with Anomalous Events")
 fig = go.Figure()
 
 #Residual line
-fig.add_trace(go.Scatter(x=residual_filtered.index,y=residual_filtered["residual"],mode="lines",name="Residual"))
+fig.add_trace(go.Scatter(x=residual_plot.index,y=residual_plot["residual"],mode="lines",name="Residual",line=dict(width=1), opacity=0.8))
 
-ae_times=ae_events_filtered["start_time"]
-ae_common_index = residual_filtered.index.intersection(ae_times)
-ae_values = residual_filtered.loc[ae_common_index, "residual"]
+ae_times=ae_events_plot["start_time"]
+ae_common_index = residual_plot.index.intersection(ae_times)
+ae_values = residual_plot.loc[ae_common_index, "residual"]
 
-fig.add_trace(go.Scatter(x=ae_common_index,y=ae_values,mode="markers",name="Autoencoder Events",marker=dict(size=7)))
+fig.add_trace(go.Scatter(x=ae_common_index,y=ae_values,mode="markers",name="Autoencoder Events",marker=dict(size=5)))
 
 if show_if:
-    if_times=if_events_filtered["start_time"]
-    if_common_index=residual_filtered.index.intersection(if_times)
-    if_values=residual_filtered.loc[if_common_index, "residual"]
-    fig.add_trace(go.Scatter(x=if_common_index,y=if_values,mode="markers",name="Isolation Forest Events",marker=dict(size=7, symbol="diamond")))
+    if_times=if_events_plot["start_time"]
+    if_common_index=residual_plot.index.intersection(if_times)
+    if_values=residual_plot.loc[if_common_index, "residual"]
+    fig.add_trace(go.Scatter(x=if_common_index,y=if_values,mode="markers",name="Isolation Forest Events",marker=dict(size=5, symbol="diamond")))
 
 if show_z:
-    z_times=z_events_filtered["start_time"]
-    z_common_index=residual_filtered.index.intersection(z_times)
-    z_values=residual_filtered.loc[z_common_index, "residual"]
-    fig.add_trace(go.Scatter(x=z_common_index,y=z_values,mode="markers",name="Z-score Events",marker=dict(size=7, symbol="x")))
+    z_times=z_events_plot["start_time"]
+    z_common_index=residual_plot.index.intersection(z_times)
+    z_values=residual_plot.loc[z_common_index, "residual"]
+    fig.add_trace(go.Scatter(x=z_common_index,y=z_values,mode="markers",name="Z-score Events",marker=dict(size=5, opacity=0.8, symbol="x")))
 
-fig.update_layout(height=600)
+fig.update_layout(height=600, xaxis=dict(rangeslider=dict(visible=True)))
 st.plotly_chart(fig, use_container_width=True)
+st.markdown(f"""**Note:** Detected anomalies may not overlap and should be viewed as complementary signals. Z-score identifies extreme deviations in magnitude, Isolation Forest detects rare observations in the feature space, and the Autoencoder captures unusual temporal patterns.""")
 st.markdown("---")
 
 #Monthly Event Counts
 st.subheader("Monthly Anomalous Events")
 fig2=go.Figure()
 
-if not ae_events_filtered.empty:
-    ae_events_plot=ae_events_filtered.copy()
+if not ae_events_plot.empty:
+    ae_events_plot=ae_events_plot.copy()
     ae_events_plot["month"]=ae_events_plot["start_time"].dt.tz_localize(None).dt.to_period("M").dt.to_timestamp()
     ae_events_per_month=ae_events_plot.groupby("month").size()
     fig2.add_trace(go.Bar(x=ae_events_per_month.index,y=ae_events_per_month.values,name="Autoencoder"))
 
-if show_if and not if_events_filtered.empty:
-    if_events_plot=if_events_filtered.copy()
+if show_if and not if_events_plot.empty:
+    if_events_plot=if_events_plot.copy()
     if_events_plot["month"]=if_events_plot["start_time"].dt.tz_localize(None).dt.to_period("M").dt.to_timestamp()
     if_events_per_month=if_events_plot.groupby("month").size()
     fig2.add_trace(go.Bar(x=if_events_per_month.index,y=if_events_per_month.values,name="Isolation Forest"))
 
-if show_z and not z_events_filtered.empty:
-    z_events_plot=z_events_filtered.copy()
+if show_z and not z_events_plot.empty:
+    z_events_plot=z_events_plot.copy()
     z_events_plot["month"]=z_events_plot["start_time"].dt.tz_localize(None).dt.to_period("M").dt.to_timestamp()
     z_events_per_month=z_events_plot.groupby("month").size()
     fig2.add_trace(go.Bar(x=z_events_per_month.index,y=z_events_per_month.values,name="Z-score"))
